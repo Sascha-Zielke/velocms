499c69cf1d623d7b62021c3d05cb95e9f8fb2af8
Sascha-Zielke <github@webzite-newmedia.de>
2026-05-25 15:46:25 +0200

    fix: load routes in index.php

--- public/index.php
+++ public/index.php
@@ -7,9 +7,14 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
 use VeloCMS\Core\App;
+use VeloCMS\Core\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 define('BASE_PATH', dirname(__DIR__));
+
+// Routen laden
+// Wir laden hier die routes.php aus dem Pages-Modul
+require_once BASE_PATH . '/modules/Pages/routes.php';
 
 App::boot();
